a272103a7a8a889c94996fbe9f43d410c862259f
maviferarj <161755121+maviferarj@users.noreply.github.com>
2024-06-29 19:34:35 -0300

    Add files via upload

---
+++ loop/index.php/index.php
@@ -0,0 +1,8 @@
+<?php 
+    $contador = 1;
+    $entrada = 10;
+    while($contador <= $entrada){
+        echo "{$contador}\n";
+        $contador += 2;
+    }
+
---
+++ loop/tabuada.while.php/tabuadawhile.php
@@ -0,0 +1,11 @@
+<?php 
+
+$numero =5;
+$contador =1;
+$res;
+
+do{
+    $res = $numero * $contador;
+    echo "$numero x $contador = {$res}\n";
+    $contador++;
+}while($contador <= 10);
